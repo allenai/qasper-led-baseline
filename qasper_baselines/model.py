1a6dee4c93474d373722fe226211604d1a4ec391
pdasigi <pradeepd@allenai.org>
2021-04-10 17:38:15 -0700

    f1 for empty evidence

--- qasper_baselines/model.py
+++ qasper_baselines/model.py
@@ -116,6 +116,7 @@ class QasperBaseline(Model):
             )
             loss_fn = torch.nn.CrossEntropyLoss(weight=weights)
             evidence_loss = loss_fn(evidence_logits.view(-1, 2), evidence.view(-1))
+            output_dict["evidence_loss"] = loss
             if loss is None:
                 loss = evidence_loss
             else:
@@ -139,12 +140,15 @@ class QasperBaseline(Model):
         for instance_predicted, instance_gold in zip(predicted_evidence_indices, gold_evidence_indices):
             instance_f1s = []
             for gold in instance_gold:
+                if sum(gold) == 0:
+                    instance_f1s.append(1.0)
+                    continue
                 # If the document was truncated to fit in the model, the gold will be longer than the 
                 # predicted indices.
                 predicted = instance_predicted + [0] * (len(gold) - len(instance_predicted))
                 true_positives = sum([i and j for i, j in zip(predicted, gold)])
                 precision = true_positives / sum(predicted) if sum(predicted) != 0 else 0.0
-                recall = true_positives / sum(gold) if sum(gold) != 0 else 0.0
+                recall = true_positives / sum(gold)
                 if precision + recall == 0:
                     instance_f1s.append(0.0)
                 else:
